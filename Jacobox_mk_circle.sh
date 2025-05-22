44ba1150ed8d0093a64f460607b20b7f0ef6f1c6
JacoboxWang <boxkinner@163.com>
2025-05-22 10:32:06 +0800

    make circle of surrounding tissue

---
+++ Jacobox_mk_circle.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+InImg=$1
+
+3dmask_tool -dilate_input 2 \
+	-inputs ${InImg} \
+	-prefix di2_${InImg}
+
+3dmask_tool -dilate_input 1 \
+	-inputs ${InImg} \
+	-prefix di1_${InImg}
+
+3dcalc -a di2_${InImg} -b di1_${InImg} -expr "a-b" -prefix tiss_${InImg}
